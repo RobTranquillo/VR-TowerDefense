e4580e0add70ab17d40e6b6dda7455d814c2598d
tranquillo <rob.tranquillo@gmx.de>
2022-04-28 08:03:14 +0200

    add; selfDestroy on destroy script

--- Assets/VR-TowerDefense/Gameplay/Scripts/Destroy.cs
+++ Assets/VR-TowerDefense/Gameplay/Scripts/Destroy.cs
@@ -5,11 +5,19 @@ using UnityEngine.Events;
 
 public class Destroy : MonoBehaviour
 {
+    [Tooltip("Destroy the whole GameObject by self after given delay. If zero no self destroy happen.")]
+    public float selfDestroyDelay = 0f;
     public UnityEvent OnDestroy;
+
+    private void Start()
+    {
+        if (selfDestroyDelay > 0)
+            Invoke("Me", selfDestroyDelay);
+    }
 
     public void Me()
     {
-        Destroy(gameObject);
         OnDestroy.Invoke();
+        Destroy(gameObject);
     }
 }
