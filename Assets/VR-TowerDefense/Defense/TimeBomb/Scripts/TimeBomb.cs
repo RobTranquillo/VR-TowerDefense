183a20a0ac34f753d6c44ce6123285bb745fdc0d
tranquillo <rob.tranquillo@gmx.de>
2022-04-28 08:03:42 +0200

    add: sound and part fx on TimeBomb

--- Assets/VR-TowerDefense/Defense/TimeBomb/Scripts/TimeBomb.cs
+++ Assets/VR-TowerDefense/Defense/TimeBomb/Scripts/TimeBomb.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,15 +7,23 @@ using UnityEngine;
 
 public class TimeBomb : MonoBehaviour
 {
+    [Header("General")]
     public float timer = 5f;
     public float killRadius = 3f;
     public int maxDamage = 250;   
+    public float triggerNewSpawnDistance = 2f;
+    [Header("FX")]
+    public AudioSource audioSource;
+    public float audioStartDelay;
+    public bool pumpingBeforeExplode = true;
+    public float pumpingInterval = 1f;
+    public GameObject explosionParticleSystemPrefab;
 
-    public float triggerNewSpawnDistance = 2f;   
-        
+
     float startTime;
     Vector3 startPos;
     bool alreadyTrigged = false;
+    GameObject explosionParticleSystem;
 
     private void OnDisable()
     {
@@ -25,6 +34,9 @@ public class TimeBomb : MonoBehaviour
     {
         startPos = transform.position;
         startTime = Time.time;
+        Invoke("EnableAudioSource", audioStartDelay);
+        Invoke("Pumping", pumpingInterval);
+        explosionParticleSystem = Instantiate(explosionParticleSystemPrefab);
     }
 
     void Update()
@@ -36,19 +48,46 @@ public class TimeBomb : MonoBehaviour
         Destroy(gameObject);
     }
 
+    private void Pumping()
+    {
+        if (!pumpingBeforeExplode)
+            return;
+
+        Vector3 defaultScale = transform.localScale;
+        Vector3 jumpHeight = transform.localPosition;
+        jumpHeight.y += 0.2f;
+        transform.DOLocalJump(jumpHeight, .5f, 2, 0.3f);
+        transform.DOShakeScale(0.3f, 1, 5, 90);
+        Invoke("Pumping", pumpingInterval);
+    }
+
+    private void EnableAudioSource()
+    {
+        if (audioSource == null)
+            return;
+        audioSource.enabled = true;
+    }
 
     private void TriggerEvent()
     {
-        var foo = Vector3.Distance(startPos, transform.position);
-        if (foo < triggerNewSpawnDistance)
+        if (Vector3.Distance(startPos, transform.position) < triggerNewSpawnDistance)
             return;
-        EventManager.current.Trigger_SpawnNewCube();
+        EventManager.current?.Trigger_SpawnNewCube();
         alreadyTrigged = true;
     }
 
     private void OnDestroy()
     {
+        ParticleBurst();
         ExplosionDamage(transform.position, killRadius, maxDamage);
+    }
+
+    private void ParticleBurst()
+    {
+        if (explosionParticleSystem == null)
+            return;
+        explosionParticleSystem.transform.position = transform.position;
+        explosionParticleSystem.SetActive(true);
     }
 
     void ExplosionDamage(Vector3 center, float radius, int dmg)
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # VR TowerDefense
 
 Jump between your towers and defend the attacs of the enemys
+
+## Audio files
+- https://opengameart.org/content/dynamite-sound-effect
